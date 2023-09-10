2a1437f7a4affb7ac95ec1af3dadd6eec6126e4d
Davide <davi9898@gmail.com>
2023-09-10 11:28:08 +0200

    ffmpeg_macos update

--- paste_video.py
+++ paste_video.py
@@ -16,11 +16,11 @@ from moviepy.editor import VideoFileClip, clips_array, concatenate_videoclips
 
 # https://zulko.github.io/moviepy/getting_started/compositing.html#compositevideoclips
 
-video1 = VideoFileClip("./server/uploads.nosync/095953.webm")
-video2 = VideoFileClip("./server/uploads.nosync/095958.webm")
-video3 = VideoFileClip("./server/uploads.nosync/100003.webm")
-video4 = VideoFileClip("./server/uploads.nosync/100008.webm")
-video5 = VideoFileClip("./server/uploads.nosync/100013.webm")
-video6 = VideoFileClip("./server/uploads.nosync/100018.webm")
-final_video = concatenate_videoclips([video1, video2, video3, video4, video5, video6])
+video1 = VideoFileClip("./server/uploads.nosync/145527.webm")
+video2 = VideoFileClip("./server/uploads.nosync/145532.webm")
+#video3 = VideoFileClip("./server/uploads.nosync/100003.webm")
+#video4 = VideoFileClip("./server/uploads.nosync/100008.webm")
+#video5 = VideoFileClip("./server/uploads.nosync/100013.webm")
+#video6 = VideoFileClip("./server/uploads.nosync/100018.webm")
+final_video = concatenate_videoclips([video1, video2])
 final_video.write_videofile("final_video.webm")
